ccb339224f78d1ef0b5f137f35977225620975f7
alibekshak <alibekshakiro1@gmail.com>
2024-01-05 16:09:59 +0600

    changing image, background. Adding animation.

--- AnimationUIKit/ViewController.swift
+++ AnimationUIKit/ViewController.swift
@@ -8,37 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var tap: UIButton!
     
     var imageView: UIImageView!
     var currentAnimation = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemIndigo.withAlphaComponent(1.5)
         
-        imageView = UIImageView(image: UIImage(named: "tiger"))
+        imageView = UIImageView(image: UIImage(named: "Cube"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(imageView)
         
         NSLayoutConstraint.activate([
-            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            imageView.widthAnchor.constraint(equalToConstant: 600),
-            imageView.heightAnchor.constraint(equalToConstant: 500),
-        ])
+               imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+               imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+           ])
     }
 
     @IBAction func tapped(_ sender: UIButton) {
-        sender.isHidden = true
+        tap.isHidden = true
         
         UIView.animate(withDuration: 1, delay: 0, animations: {
             switch self.currentAnimation {
             case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
                 break
+            case 1:
+                self.imageView.transform = .identity
             default:
                 break
             }
         }) { finished in
-            sender.isHidden = false
+            self.tap.isHidden = false
         }
         
         currentAnimation += 1
